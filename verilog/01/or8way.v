bb5320915e6497840b58dd900eaf5f359c126d95
byby9527 <99935115+byby9527@users.noreply.github.com>
2022-12-25 16:39:14 +0800

    Update and rename or_8_way.sv to or8way.v

--- verilog/01/or8way.v
+++ verilog/01/or8way.v
@@ -1,9 +1,9 @@
-`ifndef or_n2t
-  `include "or_n2t.sv"
+`ifndef or_nt
+  `include "or_nt.v"
 `endif
-`define or_8_way 1
+`define or8way 1
 
-module or_8_way(
+module or8way(
     input [7:0] in,
     output      out
 );
