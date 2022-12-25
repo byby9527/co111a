b4e8c3b378d8113d59be18825128471fafdcc6d3
byby9527 <99935115+byby9527@users.noreply.github.com>
2022-12-25 16:35:36 +0800

    Update and rename not_n2t.sv to not_nt.v

--- verilog/01/not_n2t.sv
+++
@@ -1,7 +0,0 @@
-`define not_n2t 1
-
-module not_n2t(input in, output out);
-    
-    // Put your code here
-
-endmodule
---
+++ verilog/01/not_nt.v
@@ -0,0 +1,7 @@
+`define not_nt 1
+
+module not_nt(input in, output out);
+    
+    // Put your code here
+
+endmodule
