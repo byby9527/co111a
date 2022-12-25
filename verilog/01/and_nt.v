4744ae09b3616113070a234b8051150432d8312b
byby9527 <99935115+byby9527@users.noreply.github.com>
2022-12-25 16:22:35 +0800

    Update and_nt.v

--- verilog/01/and_nt.v
+++ verilog/01/and_nt.v
@@ -1,9 +1,9 @@
 `ifndef or_nt
   `include "or_nt.v"
 `endif
-`define and_n2t 1
+`define and_nt 1
 
-module and_n2t(input a, input b, output out);
+module and_nt(input a, input b, output out);
 
     // Put your code here
 
